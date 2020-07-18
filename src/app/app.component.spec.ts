6f31af8fa353266f6ba059ef1e8452c18e38003f
Neelkumar Amin <neelkumar.amin@outlook.com>
2020-07-18 19:12:29 -0400

    dev: updating test an code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('csd app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Application is running!');
   });
 });
